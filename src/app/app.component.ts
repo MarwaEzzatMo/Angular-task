2b9f05c7be1919373e1b3036db7aae8dfabed854
MarwaEzzatMo <marwaezzat201@gmail.com>
2022-05-24 04:38:08 +0200

    day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
