b5118a5b36d4ae8b12eaab0918f13f0c28e5e02e
MarwaEzzatMo <marwaezzat201@gmail.com>
2022-05-28 15:01:21 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { AsideComponent } from './aside/aside.component';
